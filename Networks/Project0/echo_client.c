349fa47839297b8d900c2d8ed76935fb1885e7c5
Akshay Thejaswi <chickenbellyfinn@gmail.com>
2014-11-02 15:41:08 -0800

    project 0

---
+++ Networks/Project0/echo_client.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define RCVBUFSIZE 32	//size of recieve buffer
+
+void dieWithError(char* errorMessage); //error handling function
+
+int main(int argc, char** argv)
+{
+	int sock;								//socket descrriptor
+	struct sockaddr_in echoServerAddr;		//echo server address
+	unsigned short echoServerPort;			//echo server port
+	char* serverIP;							//server ip address (dotted quad)
+	char* echoString;						//string to send to server
+	char echoBuffer[RCVBUFSIZE];			//buffer for echo string
+	unsigned int echoStringLen;				//length of string to echo
+	int bytedRecieved, totalBytedRecieved;	//bytes read in single recv and total bytes read
+
+	if(argc < 3 || argc > 4){				//test for correct arguments
+		fprintf(stderr, "Usage: %s <Server IP> <Echo Word> [<Echo Port>]\n", argv[0]);
+		exit(1);
+	}
+
+	serverIP = argv[1];
+	echoString = argv[2];
+
+	if(argc == 4){
+		echoServerPort = atoi(argv[3]);		//use given port
+	} else {
+		echoServerPort = 7;					//7 is default
+	}
+
+	//create a reliable stream socket using TCP
+	if((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0){
+		dieWithError("socket() failed");
+	}
+
+	//construct server address structure
+	memset(&echoServerAddr, 0, sizeof(echoServerAddr));		//zero out structure
+	echoServerAddr.sin_family = AF_INET;					//internet address family
+	echoServerAddr.sin_addr.s_addr = inet_addr(serverIP);	//server ip address
+	echoServerAddr.sin_port - htons(echoServerPort);		//server port
+
+	//establish connection to the echo server
+	if(connect(sock, (struct sockaddr*)&echoServerAddr, sizeof(echoServerAddr)) < 0){
+		dieWithError("connect() failed");
+	}
+
+	echoStringLen = strlen(echoString);		//determine input length
+
+	//send the string to the server
+	if(send(sock, echoString, echoStringLen, 0) != echoStringLen){
+		dieWithError("send() sent a difference number of bytes than expected");
+	}
+
+	//recieve the same string back from the server
+	totalBytedRecieved = 0;
+	printf("Recieved: ");	//set to print the echoed string
+
+	while(totalBytedRecieved < echoStringLen){
+		//recieve up to the buffer size (minus 1 to leave space for the null terminator) bytes from the sender
+		if((bytedRecieved = recv(sock, echoBuffer, RCVBUFSIZE-1, 0)) <= 0){
+			dieWithError("recv() failed for connection closed prematurely");
+		}
+		totalBytedRecieved += bytedRecieved;	//keep tally of total bytes
+		echoBuffer[bytedRecieved] = '\0';		//terminate string
+		printf("%s", echoBuffer);
+
+	}
+
+	printf("\n");	//print final linefeed
+	close(socket);
+	exit(0);
+
+}
+
+void dieWithError(char* errorMessage)
+{
+	fprintf(stderr, "%s", errorMessage);
+	exit(1);
+}
---
+++ Networks/Project0/echo_server.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define MAXPENDING 5
+
+void dieWithError(char* errorMessage); //error handling function
+void handleTCPClient(int clientSocket);  //TCP client handling function
+
+int main(int argc, char** argv)
+{
+	int serverSocket;	//Socket descriptor for server
+	int clientSocket;	//Socket descriptor for client
+
+	struct sockaddr_in echoServerAddr;	//local address
+	struct sockaddr_in echoClientAddr;	//client address
+
+	unsigned short echoServerPort;	//server port
+	unsigned int clientLen;			//length of client address data structure
+
+	if(argc != 2){
+		fprintf(stderr, "Usage %s <Server Port>\n", argv[0]);
+		exit(1);
+	}
+
+	echoServerPort = atoi(argv[1]);   //first argument: local port
+
+	//create socket for incoming connections
+	if((serverSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0){
+		dieWithError("socket() failed");
+	}
+
+	//construct local address structure
+	memset(&echoServerAddr, 0, sizeof(echoServerAddr));	//zero out structure
+	echoServerAddr.sin_family = AF_INET;					//internet address family
+	echoServerAddr.sin_addr.s_addr = htonl(INADDR_ANY); //any incoming interface
+	echoServerAddr.sin_port = htons(echoServerPort);	//local port
+
+	//bind to the local address
+	if(bind(serverSocket, (struct sockaddr*)&echoServerAddr, sizeof(echoServerAddr)) < 0){
+		dieWithError("bind() failed");
+	}
+
+	//mark the socket so it will listen for incoming connections
+	if(listen(serverSocket, MAXPENDING) < 0){
+		dieWithError("listen() failed");
+	}
+
+	while(1){	//run forever
+		//set the size of the in-out parameter
+		clientLen = sizeof(echoClientAddr);
+
+		//wait for client to connect
+		if((clientSocket = accept(serverSocket, (struct sockaddr*)&echoClientAddr, clientLen)) < 0){
+			dieWithError("accept() failed");
+		}
+
+		//clientSocket is connected to a client
+		printf("Handling client %s\n", inet_ntoa(echoClientAddr.sin_addr));
+		handleTCPClient(clientSocket);
+	}
+}
+
+void handleTCPClient(int clientSocket){
+
+}
+
+void dieWithError(char* errorMessage){
+	fprintf(stderr, "%s", errorMessage);
+	exit(1);
+}
